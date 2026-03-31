f06e9eacfbe129171f5e894b207e82de08ce57fe
speedup-guru <paul@speedup.guru>
2026-03-31 11:48:38 +0400

    fix sticky bar

--- assets/n-sticky-button-variants.js
+++ assets/n-sticky-button-variants.js
@@ -7,17 +7,12 @@ document.querySelector('.add-cart-sticky-button').addEventListener('click', func
 document.querySelectorAll('.cta-bar__selector input[type="radio"]').forEach((element) => {
     element.addEventListener('click', (e) => {
         const variant_title = element.getAttribute("data-variant-title");
-
         const target = document.querySelector('input[type="radio"][id="' + variant_title + '"]');
 
         if (target) {
             if (typeof __section_landing !== 'undefined' && __section_landing) {
                 target.closest('.c-supply-selector__item').click();
                 target.checked = true;
-                purchase_form_landing_event(__section_landing, target.closest('.c-supply-selector__item'), target.closest('.c-supply-selector__item')?.dataset.product_variant_id);
-            }
-            else {
-                target.click();
             }
         }
     });
